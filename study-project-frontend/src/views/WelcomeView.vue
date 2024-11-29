5fdc76c29206c5c245f9455dd75bc7deca367f98
LiuYanchen <2532420037@qq.com>
2024-11-29 22:13:32 +0800

    登录界面

--- study-project-frontend/src/views/WelcomeView.vue
+++ study-project-frontend/src/views/WelcomeView.vue
@@ -1,34 +1,40 @@
 <template>
-    <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
-        <div style="flex: 1">
-            <el-image style="width: 100%;height: 100%" fit="cover"
-                      src="https://img1.baidu.com/it/u=4097856652,4033702227&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800"/>
-        </div>
-        <div class="welcome-title">
-            <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
-            <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流。</div>
-            <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生。</div>
-        </div>
-        <div style="width: 400px;background-color: white;z-index: 1">
-            <router-view v-slot="{ Component }">
-                <transition name="el-fade-in-linear" mode="out-in">
-                    <component :is="Component" style="height: 100%"/>
-                </transition>
-            </router-view>
-        </div>
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex; justify-content: center; align-items: center; position: relative;">
+    <!-- 背景图片 -->
+    <div style="position: absolute; width: 100%; height: 100%; z-index: -1;">
+      <el-image style="width: 100%; height: 100%" fit="cover"
+                src="https://q0.itc.cn/q_70/images01/20240422/d519b4424ad44d7eaf0ddd3a4e08448e.jpeg"/>
     </div>
+
+    <!-- 系统主题与描述，置于顶部中央 -->
+    <div class="welcome-title">
+      <div style="font-size: 35px; font-weight: bold; text-align: center;">欢迎使用 Linked Papers</div>
+      <div style="margin-top: 10px; text-align: center; font-size: 16px;">本平台汇总了来自arXiv计算机学科40个子领域的169,343篇论文</div>
+    </div>
+
+    <!-- 登录框 -->
+    <div style="width: 400px; background-color: rgba(255, 255, 255, 0.8); padding: 20px; border-radius: 10px; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);">
+      <router-view v-slot="{ Component }">
+        <transition name="el-fade-in-linear" mode="out-in">
+          <component :is="Component" style="height: 100%"/>
+        </transition>
+      </router-view>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
 .welcome-title {
-    position: absolute;
-    bottom: 30px;
-    left: 30px;
-    color: white;
-    text-shadow: 0 0 10px black;
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  color: white;
+  text-shadow: 0 0 10px black;
+  max-width: 80%;
+  text-align: center;
 }
 </style>
